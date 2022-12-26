27e6a76764ef19e5a38dfb892567a555486eb9f2
Incog89 <111927109+Incog89@users.noreply.github.com>
2022-12-26 14:34:32 -0600

    Add files via upload

---
+++ Paginas Web/EC4/phpbasic/index.php
@@ -0,0 +1,52 @@
+<?php include 'includes/header.php'?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/estilos.css" rel="stylesheet" type="text/css">
+    <title>Document</title>
+</head>
+<body>
+    <ul>
+        <li>
+            <a href="1.php">Ejercicio 1</a>
+        </li>
+        <li>
+            <a href="2.php">Ejercicio 2</a>
+        </li>
+        <li >
+            <a href="3.php">Ejercicio 3</a>
+        </li>
+        <li>
+            <a href="4.php">Ejercicio 4</a>
+        </li>
+        <li>
+            <a href="5.php">Ejercicio 5</a>
+        </li>
+        <li>
+            <a href="6.php">Ejercicio 6</a>
+        </li>
+        <li>
+            <a href="7.php">Ejercicio 7</a>
+        </li>
+        <li>
+            <a href="8.php">Ejercicio 8</a>
+        </li>
+        <li>
+            <a href="9.php">Ejercicio 9</a>
+        </li>
+        <li>
+            <a href="10.php">Ejercicio 10</a>
+        </li>
+        <li>
+            <a href="interfaz2.php">Ejercicio Interfaz 2</a>
+        </li>
+        <li>
+            <a href="interfaz3.php">Aplicativos de calculos</a>
+        </li>
+    </ul>
+</body>
+</html>
+
